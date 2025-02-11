f81f1995e6a0fc76c8efe9fb3bcf8b7b9d53312a
Pradise2 <akinsunmitaye@gmail.com>
2025-02-11 01:23:01 +0100

    Your commit message

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,3 @@
-// import { createAppKit } from "@reown/appkit/react";
-// import { WagmiProvider } from "wagmi";
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-// import { projectId, metadata, networks, wagmiAdapter } from "./config/config";
-// import Navbar from "./components/Navbar";
-// import GetGameState from "./components/GetGameState";
-// import "./App.css";
-// import CreateGame from "./components/CreateGame";
-// import Available from "./components/Available";
-// // import MyGamesSection from "./components/MyGames";
-
-// const queryClient = new QueryClient();
-
-// const generalConfig = {
-//   projectId,
-//   metadata,
-//   networks,
-// };
-
-// // Create modal
-// createAppKit({
-//   adapters: [wagmiAdapter],
-//   ...generalConfig,
-// });
-
-// export function App() {
-//   return (
-//     <div className=" min-h-screen bg-gradient-to-b from-slate-900 to-slate-800">
-//       <Navbar />
-//       <CreateGame />
-//       {/* <MyGamesSection /> */}
-//       <Available />
-//       <div className="container mx-auto px-4 py-8">
-//         <WagmiProvider config={wagmiAdapter.wagmiConfig}>
-//           <QueryClientProvider client={queryClient}>
-//             <GetGameState />
-//           </QueryClientProvider>
-//         </WagmiProvider>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { createAppKit } from "@reown/appkit/react";
@@ -51,15 +6,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { projectId, metadata, networks, wagmiAdapter } from "./config/config";
 import { ApolloProvider } from "@apollo/client";
 import client from "./components/PvpSection/client/apollo-client";
-
-
 import HomePage from "./components/HomePage";
-
-
-
-
 import "./App.css";
-
 import Pvp from "./components/PvpSection/Pvp";
 
 const queryClient = new QueryClient();
